a0324243599b839c81e68c16906bf4469061af06
Amanda Georgescu <georgescu.amanda@gmail.com>
2017-01-20 20:35:05 -0500

    fix indent

--- sha256.py
+++ sha256.py
@@ -43,5 +43,5 @@ def get_message(sha256):
 if __name__ == '__main__':
 	db.create_all()
 	port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
 
